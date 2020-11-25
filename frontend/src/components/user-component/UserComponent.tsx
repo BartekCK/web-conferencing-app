26a5462c011ef8c4b08209abcd61785fae877c8a
Bartlomiej Kotarski <bkotarski1@gmail.com>
2020-11-25 23:35:09 +0100

    Lista roomow

--- frontend/src/components/user-component/UserComponent.tsx
+++ frontend/src/components/user-component/UserComponent.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import CustomAvatar from 'components/avatar/Avatar';
-import { UserComponentStyled } from 'components/user-component/styles';
-import { Modal } from 'antd';
+
+// hooks
 import { useTranslation } from 'react-i18next';
+
+// components
 import SettingsComponent from 'components/settings';
+import RoomContainer from 'components/room-container';
+import CustomAvatar from 'components/avatar/Avatar';
+import { Modal } from 'antd';
 
 const UserComponent: React.FC = () => {
     const [isModalVisible, setModalVisible] = React.useState<boolean>(false);
@@ -20,12 +24,11 @@ const UserComponent: React.FC = () => {
 
     return (
         <React.Fragment>
-            <UserComponentStyled>
-                <CustomAvatar
-                    imgSrc="https://store-images.s-microsoft.com/image/apps.23952.13510798887611042.610ae026-cc3d-4b4e-9044-1b8721988d93.876c0225-be52-4dab-849f-dee26d8f83ab?mode=scale&q=90&h=270&w=270&background=%23107C10"
-                    handleClick={handleAvatarClick}
-                />
-            </UserComponentStyled>
+            <CustomAvatar
+                imgSrc="https://store-images.s-microsoft.com/image/apps.23952.13510798887611042.610ae026-cc3d-4b4e-9044-1b8721988d93.876c0225-be52-4dab-849f-dee26d8f83ab?mode=scale&q=90&h=270&w=270&background=%23107C10"
+                handleClick={handleAvatarClick}
+            />
+            <RoomContainer />
             {isModalVisible && (
                 <Modal
                     title={t('common.settings')}
