bc8979b84976cc740f63209ba92e6ae6bd2671ed
Bartlomiej Kotarski <bkotarski1@gmail.com>
2020-11-26 22:09:30 +0100

    Filtrowanie wynikow roomow

--- frontend/src/components/room-container/RoomContainer.tsx
+++ frontend/src/components/room-container/RoomContainer.tsx
@@ -11,17 +11,20 @@ import { ISingleRoom } from './types';
 
 // components
 import SingleRoom from './components/single-room';
-import { Input, Spin } from 'antd';
+import { Empty, Input, Spin } from 'antd';
 
 // icons
 import { SearchOutlined } from '@ant-design/icons';
 
 // hooks
 import { useTranslation } from 'react-i18next';
+import useDelayExecution from 'custom--hooks/useDelayExecution';
 
 const RoomContainer: React.FC = () => {
     const [rooms, setRooms] = React.useState<ISingleRoom[]>([]);
     const [isLoading, setLoading] = React.useState<boolean>(false);
+    const [filterList, setFilterList] = React.useState<ISingleRoom[]>([]);
+    const [inputValue, setInputValue] = React.useState<string>('');
     const { t } = useTranslation();
 
     React.useEffect(() => {
@@ -31,37 +34,75 @@ const RoomContainer: React.FC = () => {
             temp.push({
                 id: uuidv4(),
                 roomCode: uuidv4().slice(0, -18),
-                name: 'Ala ma kota',
+                name: uuidv4().slice(32),
             });
         }
         setRooms(temp);
     }, []);
 
+    React.useEffect(() => {
+        if (!inputValue) {
+            setFilterList([]);
+        }
+    }, [inputValue]);
+
+    const searchInList = async () => {
+        setLoading(true);
+        const temp = () => new Promise((resolve) => setTimeout(resolve, 1000));
+        await temp();
+        const result: ISingleRoom[] = rooms.filter((room: ISingleRoom) => {
+            return Object.keys(room).some((value: string) => room[value].includes(inputValue));
+        });
+        setFilterList(result);
+    };
+
+    const finallyCallback = () => {
+        setLoading(false);
+    };
+    const [ref] = useDelayExecution(searchInList, 500, finallyCallback);
+
     const handleChange = (event) => {
-        console.log(event.target.value);
+        const { value } = event.target;
+        setInputValue(value);
+    };
+
+    const renderSingleRoom = () => {
+        if (filterList.length > 0) {
+            return filterList.map(({ roomCode, name, id }: ISingleRoom) => (
+                <SingleRoom key={id} id={id} name={name} roomCode={roomCode} />
+            ));
+        }
+        if (rooms.length > 0 && inputValue === '') {
+            return rooms.map(({ roomCode, name, id }: ISingleRoom) => (
+                <SingleRoom key={id} id={id} name={name} roomCode={roomCode} />
+            ));
+        }
+        return (
+            <Empty
+                description={
+                    <button
+                        type="button"
+                        className="ant-btn ant-btn-primary "
+                    >
+                        {t('messages.emptyResult')}
+                    </button>
+                }
+            />
+        );
     };
 
     return (
         <RoomContainerStyled>
-            <Input
-                className="mb-2"
-                placeholder={`${t('common.search')} ...`}
-                prefix={<SearchOutlined />}
-                onChange={handleChange}
-                allowClear
-            />
+            <div ref={ref}>
+                <Input
+                    className="mb-2"
+                    placeholder={`${t('common.search')} ...`}
+                    prefix={<SearchOutlined />}
+                    onChange={handleChange}
+                />
+            </div>
             <Spin spinning={isLoading}>
-                <div className="rooms--wrapper">
-                    {rooms.length > 0
-                        && rooms.map(({ roomCode, name, id }: ISingleRoom) => (
-                            <SingleRoom
-                                key={id}
-                                id={id}
-                                name={name}
-                                roomCode={roomCode}
-                            />
-                        ))}
-                </div>
+                <div className="rooms--wrapper">{renderSingleRoom()}</div>
             </Spin>
         </RoomContainerStyled>
     );
--- frontend/src/components/room-container/styles.ts
+++ frontend/src/components/room-container/styles.ts
@@ -3,8 +3,9 @@ import styled from 'styled-components';
 const RoomContainerStyled = styled.div`
     .rooms--wrapper {
         background: rgba(255, 255, 255, 0.4);
-        height: 500px;
+        max-height: 500px;
         overflow: auto;
+        min-width: 350px;
     }
 `;
 
